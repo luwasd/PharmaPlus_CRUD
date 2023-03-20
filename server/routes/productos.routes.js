4e85f51cf52cc1ab6d00d4b81728d8ccfd7e73e7
luwasd <lmanuel14@gmail.com>
2023-03-19 23:18:11 -0300

    modelo productos modificado y agregado el controlador addProductos

--- server/routes/productos.routes.js
+++ server/routes/productos.routes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const controllers = require('../controllers/productos.controllers');
+const admin = require('../middlewares/admin');
 
 const productosRouter = () => {
     const router = express.Router();
     //metodos del controlador (post, get, put, delete)
     router.get('/productos', controllers.getProductos);
-    // router.post('/', controllers.create); aun no se usa pero quiero agreagar mas adelante manejo de stcok y agregar productos
+    router.post('/productos', admin, controllers.addProductos); //aun no se usa pero quiero agreagar mas adelante manejo de stcok y agregar productos
     // router.put('/:id', controllers.update);
     // router.delete('/:id', controllers.remove);
 
