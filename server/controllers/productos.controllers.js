b478a5d2241776472b32b3615fa1c708f65e8fb0
luwasd <lmanuel14@gmail.com>
2023-03-21 22:55:21 -0300

    funcion de eliminar Admin

--- server/controllers/productos.controllers.js
+++ server/controllers/productos.controllers.js
@@ -38,4 +38,26 @@ const addProductos = async (req, res) => {
 
 };
 
-module.exports = { getProductos, addProductos };
+const deleteProducto = async (req, res) => {
+    const { productoId } = req.params;
+
+    const esProducto = await Producto.findById(productoId); //buscar si existe el producto en la base de datos
+
+    if (esProducto) {
+        await Producto.findByIdAndDelete(productoId)
+            .then(() =>
+                res.json({
+                    mensaje: "Se ha eliminado el producto con exito",
+                })
+            )
+            .catch((err) =>
+                res
+                    .status(400)
+                    .json({ mensaje: "No se ha podido eliminar el producto" })
+            );
+    } else {
+        res.json({ mensaje: "El producto no existe" });
+    }
+};
+
+module.exports = { getProductos, addProductos, deleteProducto };
